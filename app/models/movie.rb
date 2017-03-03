a6eba65cd0a9283ecfed47c1f5586088df75f12f
gorajski <nickgorajski@gmail.com>
2017-03-02 22:37:28 -0600

    Adds movies#index

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,6 +8,16 @@ class Movie < ApplicationRecord
 	has_many :auditoriums, through: :showtimes
 	has_many :sales, through: :showtimes
 
+	def unique_showdates
+		showtimes.group(:date).map { |showtime| showtime.date }
+	end
 
-
+	def showtimes_on_date(date)
+		showtimes.where(date: date)
+	end
+	
 end
+
+			# <% movie.showtimes.order(:time).each do |showtime| %>
+			# 	<li><%= showtime.time.strftime("%l:%M %p") %></li>
+			# <% end %>
