81a9fce90f6ff13057a213756042f7bb56bc458d
gorajski <nickgorajski@gmail.com>
2017-03-05 12:35:03 -0600

    Adds showtimes#new and showtimes#create.

--- app/controllers/showtimes_controller.rb
+++ app/controllers/showtimes_controller.rb
@@ -13,12 +13,30 @@ class ShowtimesController < ApplicationController
 		end 
 	end
 
+	def new
+		@showtime = Showtime.new
+		render "new"
+	end
+
+	def create
+		@showtime = Showtime.new(showtime_params)
+		if @showtime.save
+			render "show"
+		else
+			@errors = @showtime.errors.full_messages
+			render "new"
+		end
+	end
+
+
+
+
 
 
 	private
 
 	def showtime_params
-    params.require(:showtime).permit(:time)
+    params.require(:showtime).permit(:date, :time, :movie_id, :auditorium_id)
   end
 	
 end
