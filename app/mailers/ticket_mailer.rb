03b882b4f503fa10ddd6952c2689ab292e92d493
gorajski <nickgorajski@gmail.com>
2017-03-08 12:07:09 -0600

    Fixes bug in email templates.

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -1,8 +1,9 @@
 class TicketMailer < ApplicationMailer
 	default from: 'dummy_acct@dummy_domain.com'
 
-	def ticket_email(customer)
+	def ticket_email(customer, sale)
 		@customer = customer
+		@sale = sale
 		mail(to: @customer.email, subject: "Popcorn Cinema - Your Ticket")
 	end
 end
